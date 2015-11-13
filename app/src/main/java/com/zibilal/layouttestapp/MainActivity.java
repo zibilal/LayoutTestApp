65000e1c7d69835c864a53582a677166e23d05a3
Bilal <bmuhamm@gmail.com>
2015-11-13 19:22:55 +0700

    Third commit

--- app/src/main/java/com/zibilal/layouttestapp/MainActivity.java
+++ app/src/main/java/com/zibilal/layouttestapp/MainActivity.java
@@ -1,13 +1,30 @@
 package com.zibilal.layouttestapp;
 
+import android.graphics.Color;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.zibilal.layouttestapp.customs.IcoMoonDrawable;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.fab)
+    FloatingActionButton mFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        IcoMoonDrawable checkDrawable = new IcoMoonDrawable(getApplicationContext(), R.string.icon_checkmark);
+        checkDrawable.setDPSize(12);
+        checkDrawable.setColor(Color.CYAN);
+        mFab.setImageDrawable(checkDrawable);
     }
 }
